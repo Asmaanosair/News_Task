496835a5f8bd549e7ee5311d139fdd01e281cd06
Asmaa Nosair <asmaanosair5@gmail.com>
2025-11-10 22:43:13 +0200

    add:limiter

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,6 @@
 use App\Http\Controllers\Api\ArticleController;
 use Illuminate\Support\Facades\Route;
 
-Route::prefix('articles')->group(function () {
+Route::prefix('articles')->middleware('throttle:60,1')->group(function () {
     Route::get('/', [ArticleController::class, 'index']);
 });
